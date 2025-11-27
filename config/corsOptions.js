d612d227c939e71d364f2b3941a5879e3000c61b
Mahmoud Elsebaey <mahmoudelsebaey710@gmail.com>
2025-11-27 11:09:01 +0200

    test2

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,14 +2,12 @@ import { allowedOrigins } from "./allowedOrigins.js";
 
 export const corsOptions = {
   origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
     }
   },
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
   optionsSuccessStatus: 200,
 };
