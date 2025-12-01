a67ea781094f2b834a3e22c8ca535dd8f7fe7808
Mahmoud Elsebaey <mahmoudelsebaey710@gmail.com>
2025-12-01 07:54:50 +0200

    test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import { connectionDB } from "./config/db/connectionDB.js";
 import { config } from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
-// import { corsOptions } from "./config/corsOptions.js";
+import { corsOptions } from "./config/corsOptions.js";
 import cookieParser from "cookie-parser";
 import * as routes from "./src/Routes/Root.route.js";
 import path from "path";
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.json());
 // ________________________________ CORS configuration and middleware setup ________________________________
 const corsOptions = {
-  origin: "https://authentication-app-frontend-seven.vercel.app",
+  origin: process.env.FRONTEND_URL_VERCEL || "http://localhost:3000",
   credentials: true,
   optionsSuccessStatus: 200,
 };
@@ -33,12 +33,10 @@ app.options("*", cors(corsOptions)); // Enable pre-flight for all routes
 
 // ________________________________ Serve static files from the "public" directory ________________________________
 app.use(express.static(path.join(__dirname, "public")));
-
 // ________________________________ Basic route ________________________________
 app.use("/", routes.rootRouter);
 app.use("/auth", routes.authRouter);
 app.use("/users", routes.userRouter);
-
 // ________________________________ 404 Error handling for unmatched routes ________________________________
 app.use((req, res) => {
   res.status(404);
@@ -50,20 +48,17 @@ app.use((req, res) => {
     res.type("txt").send("404 Not Found");
   }
 });
-
 // ________________________________ Error handling middleware ________________________________
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
 });
-
 // ________________________________ Start the server after successful DB connection ________________________________
 mongoose.connection.once("open", () => {
   app.listen(PORT, "0.0.0.0", () =>
     console.log(`Server running on port ${PORT}`)
   );
 });
-
 // ________________________________ Handle DB connection errors ________________________________
 mongoose.connection.on("error", (err) => {
   console.log(err);
